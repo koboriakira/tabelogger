801875ff93378d77a697a92eb02cc844498297c8
Akira Kobori <private.beats@gmail.com>
2020-09-03 11:32:20 +0900

    別オリジンからのAPI呼び出しを許可する

--- tabelogger/main.py
+++ tabelogger/main.py
@@ -1,11 +1,21 @@
-from tabelogger.adapter.logger.fastapi_logging import FastapiLogging
-from tabelogger.model.store import Stores
 import uuid
 from fastapi import FastAPI, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
 from tabelogger.job.background_tabelog_scraper import BackgroundTabelogScraper
 from tabelogger.adapter.data_object.mysql_data_object import MySqlDataObject
+from tabelogger.adapter.logger.fastapi_logging import FastapiLogging
+from tabelogger.model.store import Stores
 
 app = FastAPI()
+
+# CORSを許可
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 
 
 @app.get("/")
